b575b73d46f05b765e09b332142fcc3ed755b407
Shobhit Bakliwal <shobhitic@gmail.com>
2017-10-26 21:43:05 +0530

    Created a Add screen

---
+++ To Do List/AddItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddItemViewController.swift
+//  To Do List
+//
+//  Created by Shobhit Bakliwal on 26/10/17.
+//  Copyright © 2017 Shobhit Bakliwal. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
