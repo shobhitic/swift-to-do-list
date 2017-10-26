059ff0f186135136bcf72530d8e62a122d3edd00
Shobhit Bakliwal <shobhitic@gmail.com>
2017-10-26 20:00:50 +0530

    Added a ToDoItemTableViewCell and set the Table View Cell to it.

---
+++ To Do List/ToDoItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoItemTableViewCell.swift
+//  To Do List
+//
+//  Created by Shobhit Bakliwal on 26/10/17.
+//  Copyright © 2017 Shobhit Bakliwal. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
